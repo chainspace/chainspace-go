36dd5176eaf3cd75d891b01c2dfbc0315bf1ae32
tav <tav@espians.com>
2018-07-02 01:15:50 +0100

    node: add support for peer auth and reworked config

--- node/node.go
+++ node/node.go
@@ -1,43 +1,47 @@
 package node // import "chainspace.io/prototype/node"
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
-	"encoding/binary"
+	"encoding/base32"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
-	"github.com/lucas-clemente/quic-go"
-
 	"chainspace.io/prototype/config"
+	"chainspace.io/prototype/crypto/signature"
 	"chainspace.io/prototype/freeport"
 	"chainspace.io/prototype/network"
 	"chainspace.io/prototype/service"
 	"chainspace.io/prototype/service/broadcast"
-	"chainspace.io/prototype/state"
 	"chainspace.io/prototype/storage"
 	"chainspace.io/prototype/storage/memstore"
+	"github.com/gogo/protobuf/proto"
+	"github.com/lucas-clemente/quic-go"
 	"github.com/tav/golly/log"
 )
 
 const (
-	initialBackoff = 2 * time.Second
-	dirPerms       = 0700
-	maxBackoff     = 30 * time.Second
-	maxMessage     = 1 << 27 // 128MB
-	readTimeout    = 30 * time.Second
-	writeTimeout   = 30 * time.Second
+	dirPerms = 0700
 )
 
 // Error values.
 var (
-	ErrDirectoryMissing = errors.New("node: config is missing a value for Directory")
+	errDirectoryMissing = errors.New("node: config is missing a value for Directory")
+	errInvalidSignature = errors.New("node: invalid signature on hello")
 )
+
+var b32 = base32.StdEncoding.WithPadding(base32.NoPadding)
+
+type usedNonce struct {
+	nonce []byte
+	ts    time.Time
+}
 
 // Config represents the configuration of an individual node within a Chainspace network.
 type Config struct {
@@ -51,16 +55,23 @@ type Config struct {
 
 // Server represents a running Chainspace node.
 type Server struct {
-	broadcaster  *broadcast.Service
-	cancel       context.CancelFunc
-	db           storage.DB
-	ctx          context.Context
-	id           uint64
-	readTimeout  time.Duration
-	sharder      service.Handler
-	top          *network.Topology
-	transactor   service.Handler
-	writeTimeout time.Duration
+	broadcaster    *broadcast.Service
+	cancel         context.CancelFunc
+	db             storage.DB
+	ctx            context.Context
+	id             uint64
+	initialBackoff time.Duration
+	key            signature.KeyPair
+	keys           map[uint64]signature.PublicKey
+	readTimeout    time.Duration
+	maxBackoff     time.Duration
+	maxClockSkew   time.Duration
+	mu             sync.RWMutex // protects nonceMap
+	nonceMap       map[uint64][]usedNonce
+	sharder        service.Handler
+	top            *network.Topology
+	transactor     service.Handler
+	writeTimeout   time.Duration
 }
 
 func (s *Server) handleConnection(conn quic.Session) {
@@ -76,38 +87,47 @@ func (s *Server) handleConnection(conn quic.Session) {
 
 func (s *Server) handleStream(stream quic.Stream) {
 	defer stream.Close()
-	buf := make([]byte, 1)
-	if _, err := stream.Read(buf); err != nil {
-		log.Errorf("Unable to read stream type: %s", err)
+	c := network.NewConn(stream)
+	hello, err := c.ReadHello(s.readTimeout)
+	if err != nil {
+		log.Errorf("Unable to read hello message from stream: %s", err)
 		return
 	}
-	var svc service.Handler
-	switch buf[0] {
-	case 1:
+	var (
+		svc    service.Handler
+		peerID uint64
+	)
+	switch hello.Type {
+	case service.CONNECTION_BROADCAST:
 		svc = s.broadcaster
-	case 2:
+		peerID, err = s.verifyPeerID(hello)
+		if err != nil {
+			log.Errorf("Unable to verify peer ID from the hello message: %s", err)
+			return
+		}
+	case service.CONNECTION_TRANSACTOR:
 		svc = s.transactor
 	default:
-		log.Errorf("Unknown stream type: %#v", buf[0])
-		if err := stream.Close(); err != nil {
-			log.Errorf("Received error on attempt to close stream: %s", err)
-		}
+		log.Errorf("Unknown connection type: %#v", hello.Type)
 		return
 	}
 	ctx := stream.Context()
 	for {
-		msg, err := s.readMessage(stream)
+		msg, err := c.ReadMessage(s.readTimeout)
 		if err != nil {
 			log.Errorf("Could not decode message from an incoming stream: %s", err)
 			return
 		}
-		resp, err := svc.Handle(ctx, msg)
+		resp, err := svc.Handle(ctx, peerID, msg)
 		if err != nil {
 			log.Errorf("Received error response from the %s service: %s", svc.Name(), err)
 			return
 		}
 		if resp != nil {
-			// send back response
+			if err = c.WritePayload(resp, s.writeTimeout); err != nil {
+				log.Errorf("Unable to write response to peer %d: %s", peerID, err)
+				return
+			}
 		}
 	}
 
@@ -124,22 +144,22 @@ func (s *Server) listen(l quic.Listener) {
 	}
 }
 
-func (s *Server) maintainBroadcast(ctx context.Context, peerID uint64) {
-	backoff := initialBackoff
+func (s *Server) maintainBroadcast(peerID uint64) {
+	backoff := s.initialBackoff
 	for {
 		select {
 		case <-s.ctx.Done():
 			break
 		default:
 		}
-		conn, err := s.top.Dial(ctx, peerID)
+		conn, err := s.top.Dial(s.ctx, peerID)
 		if err == nil {
-			backoff = initialBackoff
+			backoff = s.initialBackoff
 		} else {
 			log.Errorf("Couldn't dial %d: %s", peerID, err)
 			backoff *= 2
-			if backoff > maxBackoff {
-				backoff = maxBackoff
+			if backoff > s.maxBackoff {
+				backoff = s.maxBackoff
 			}
 			time.Sleep(backoff)
 			continue
@@ -165,38 +185,77 @@ func (s *Server) maintainBroadcast(ctx context.Context, peerID uint64) {
 	}
 }
 
-func (s *Server) readMessage(stream quic.Stream) (*service.Message, error) {
-	buf := make([]byte, 4)
-	need := 4
-	for need > 0 {
-		stream.SetReadDeadline(time.Now().Add(s.readTimeout))
-		n, err := stream.Read(buf[4-need:])
-		if err != nil {
-			log.Errorf("Got error reading from stream: %s", err)
-			return nil, err
+func (s *Server) pruneNonceMap() {
+	maxSkew := s.maxClockSkew
+	tick := time.NewTicker(maxSkew)
+	var xs []usedNonce
+	for {
+		select {
+		case <-tick.C:
+			now := time.Now()
+			nmap := map[uint64][]usedNonce{}
+			s.mu.Lock()
+			for nodeID, nonces := range s.nonceMap {
+				xs = nil
+				for _, used := range nonces {
+					diff := now.Sub(used.ts)
+					if diff < 0 {
+						diff = -diff
+					}
+					if diff < maxSkew {
+						xs = append(xs, used)
+					}
+				}
+				if xs != nil {
+					nmap[nodeID] = xs
+				}
+			}
+			s.nonceMap = nmap
+			s.mu.Unlock()
+		case <-s.ctx.Done():
+			tick.Stop()
+			return
 		}
-		need -= n
 	}
-	// TODO(tav): Fix for 32-bit systems.
-	size := int(binary.LittleEndian.Uint32(buf))
-	if size > maxMessage {
-		log.Errorf("Message size of %d exceeds the max message size", size)
-		return nil, fmt.Errorf("node: message size %d exceeds max message size", size)
+}
+
+func (s *Server) verifyPeerID(hello *service.Hello) (uint64, error) {
+	if len(hello.Payload) == 0 {
+		return 0, nil
 	}
-	buf = make([]byte, size)
-	need = size
-	for need > 0 {
-		stream.SetReadDeadline(time.Now().Add(s.readTimeout))
-		n, err := stream.Read(buf[size-need:])
-		if err != nil {
-			log.Errorf("Got error reading from stream: %s", err)
-			return nil, err
+	info := &service.HelloInfo{}
+	if err := proto.Unmarshal(hello.Payload, info); err != nil {
+		return 0, err
+	}
+	if info.Server != s.id {
+		return 0, fmt.Errorf("node: mismatched server field in hello payload: expected %d, got %d", s.id, info.Server)
+	}
+	key, exists := s.keys[info.Client]
+	if !exists {
+		return 0, fmt.Errorf("node: could not find public signing key for node %d", info.Client)
+	}
+	if !key.Verify(hello.Payload, hello.Signature) {
+		return 0, fmt.Errorf("node: could not validate signature claiming to be from node %d", info.Client)
+	}
+	diff := time.Now().Sub(info.Timestamp)
+	if diff < 0 {
+		diff = -diff
+	}
+	if diff > s.maxClockSkew {
+		return 0, fmt.Errorf("node: timestamp in client hello is outside of the max clock skew range: %d", diff)
+	}
+	s.mu.RLock()
+	nonces, exists := s.nonceMap[info.Client]
+	if exists {
+		for _, used := range nonces {
+			if bytes.Equal(used.nonce, info.Nonce) {
+				s.mu.RUnlock()
+				return 0, fmt.Errorf("node: received duplicate nonce from %d", info.Client)
+			}
 		}
-		need -= n
 	}
-	msg := &service.Message{}
-	err := proto.Unmarshal(buf, msg)
-	return msg, err
+	s.mu.RUnlock()
+	return info.Client, nil
 }
 
 // AddTransaction adds the provided raw transaction data to the node's
@@ -205,7 +264,13 @@ func (s *Server) readMessage(stream quic.Stream) (*service.Message, error) {
 // 	s.transactor.AddTransaction(txdata)
 // }
 
-// Shutdown closes all underlying connections associated with the node.
+// AddTransactionData is a temporary method for testing. It adds the given
+// transaction to the node's current block.
+func (s *Server) AddTransactionData(txdata *broadcast.TransactionData) {
+	s.broadcaster.AddTransaction(txdata)
+}
+
+// Shutdown closes all underlying resources associated with the node.
 func (s *Server) Shutdown() {
 	s.cancel()
 }
@@ -218,7 +283,7 @@ func Run(cfg *Config) (*Server, error) {
 	// Initialise the runtime directory for the node.
 	dir := cfg.Directory
 	if dir == "" {
-		return nil, ErrDirectoryMissing
+		return nil, errDirectoryMissing
 	}
 	if !filepath.IsAbs(dir) {
 		if dir, err = filepath.Abs(dir); err != nil {
@@ -243,27 +308,27 @@ func Run(cfg *Config) (*Server, error) {
 	}
 
 	// Bootstrap using a contacts JSON file.
-	if cfg.Node.BootstrapFile != "" {
-		if err = top.BootstrapFile(cfg.Node.BootstrapFile); err != nil {
+	if cfg.Node.Bootstrap.File != "" {
+		if err = top.BootstrapFile(cfg.Node.Bootstrap.File); err != nil {
 			return nil, err
 		}
 	}
 
 	// Bootstrap using mDNS.
-	if cfg.Node.BootstrapMDNS {
+	if cfg.Node.Bootstrap.MDNS {
 		top.BootstrapMDNS()
 	}
 
 	// Bootstrap using a static map of addresses.
-	if cfg.Node.BootstrapStatic != nil {
-		if err = top.BootstrapStatic(cfg.Node.BootstrapStatic); err != nil {
+	if cfg.Node.Bootstrap.Static != nil {
+		if err = top.BootstrapStatic(cfg.Node.Bootstrap.Static); err != nil {
 			return nil, err
 		}
 	}
 
 	// Bootstrap using a URL endpoint.
-	if cfg.Node.BootstrapURL != "" {
-		if err = top.BootstrapURL(cfg.Node.BootstrapURL); err != nil {
+	if cfg.Node.Bootstrap.URL != "" {
+		if err = top.BootstrapURL(cfg.Node.Bootstrap.URL); err != nil {
 			return nil, err
 		}
 	}
@@ -274,7 +339,7 @@ func Run(cfg *Config) (*Server, error) {
 		return nil, err
 	}
 
-	// Initialise the TLS cert
+	// Initialise the TLS cert.
 	cert, err := tls.X509KeyPair([]byte(cfg.Keys.TransportCert.Public), []byte(cfg.Keys.TransportCert.Private))
 	if err != nil {
 		return nil, fmt.Errorf("node: could not load the X.509 transport.cert from config: %s", err)
@@ -283,7 +348,29 @@ func Run(cfg *Config) (*Server, error) {
 		Certificates: []tls.Certificate{cert},
 	}
 
-	// Initialise the datastore
+	// Initialise signing keys.
+	var key signature.KeyPair
+	switch cfg.Keys.SigningKey.Type {
+	case "ed25519":
+		pubkey, err := b32.DecodeString(cfg.Keys.SigningKey.Public)
+		if err != nil {
+			return nil, fmt.Errorf("node: could not decode the base32-encoded public signing.key from config: %s", err)
+		}
+		privkey, err := b32.DecodeString(cfg.Keys.SigningKey.Private)
+		if err != nil {
+			return nil, fmt.Errorf("node: could not decode the base32-encoded private signing.key from config: %s", err)
+		}
+		key, err = signature.LoadKeyPair(signature.Ed25519, append(pubkey, privkey...))
+		if err != nil {
+			return nil, fmt.Errorf("node: unable to load the signing.key from config: %s", err)
+		}
+	default:
+		return nil, fmt.Errorf("node: unknown type of signing.key found in config: %q", cfg.Keys.SigningKey.Type)
+	}
+
+	keys := top.SeedPublicKeys()
+
+	// Initialise the datastore.
 	var db storage.DB
 
 	switch cfg.Node.Storage.Type {
@@ -295,38 +382,51 @@ func Run(cfg *Config) (*Server, error) {
 		return nil, fmt.Errorf("node: unknown storage type: %q", cfg.Node.Storage.Type)
 	}
 
-	state := state.Load(db)
-	broadcaster := broadcast.New(&broadcast.Config{NodeID: cfg.NodeID}, top, state)
+	shardID := top.ShardForNode(cfg.NodeID)
+	nodes := top.NodesInShard(shardID)
+	if len(nodes) == 0 {
+		return nil, fmt.Errorf("node: got no peers for the node %d", cfg.NodeID)
+	}
+
+	idx := 0
+	peers := make([]uint64, len(nodes)-1)
+	for _, peer := range nodes {
+		if peer != cfg.NodeID {
+			peers[idx] = peer
+			idx++
+		}
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
+	bcfg := &broadcast.Config{
+		ConsensusInterval: cfg.Node.Consensus.Interval,
+		Key:               key,
+		Keys:              keys,
+		NodeID:            cfg.NodeID,
+		Peers:             peers,
+	}
+
+	broadcaster := broadcast.New(ctx, bcfg, top, db)
 	node := &Server{
-		broadcaster: broadcaster,
-		cancel:      cancel,
-		ctx:         ctx,
-		db:          db,
-		id:          cfg.NodeID,
-		top:         top,
-	}
-
-	if cfg.Node.ConnectionReadTimeout == 0 {
-		node.readTimeout = readTimeout
-	} else {
-		node.readTimeout = cfg.Node.ConnectionReadTimeout
-	}
-
-	if cfg.Node.ConnectionWriteTimeout == 0 {
-		node.writeTimeout = writeTimeout
-	} else {
-		node.writeTimeout = cfg.Node.ConnectionWriteTimeout
+		broadcaster:    broadcaster,
+		cancel:         cancel,
+		ctx:            ctx,
+		db:             db,
+		id:             cfg.NodeID,
+		initialBackoff: cfg.Node.Broadcast.InitialBackoff,
+		key:            key,
+		keys:           keys,
+		maxBackoff:     cfg.Node.Broadcast.MaxBackoff,
+		maxClockSkew:   cfg.Node.Broadcast.MaxClockSkew,
+		nonceMap:       map[uint64][]usedNonce{},
+		readTimeout:    cfg.Node.Connections.ReadTimeout,
+		top:            top,
+		writeTimeout:   cfg.Node.Connections.WriteTimeout,
 	}
 
 	// Maintain persistent connections to all other nodes in our shard.
-	shardID := top.ShardForNode(cfg.NodeID)
-	peers := top.NodesInShard(shardID)
-	for _, peerID := range peers {
-		if peerID != cfg.NodeID {
-			go node.maintainBroadcast(node.ctx, peerID)
-		}
+	for _, peer := range peers {
+		go node.maintainBroadcast(peer)
 	}
 
 	// Start listening on the given port.
@@ -336,6 +436,7 @@ func Run(cfg *Config) (*Server, error) {
 		return nil, fmt.Errorf("node: unable to listen on port %d: %s", port, err)
 	}
 
+	go node.pruneNonceMap()
 	go node.listen(l)
 
 	// Announce our location to the world.
