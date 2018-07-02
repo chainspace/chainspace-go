4dbf695205d16c8b5cfe4482ca1d4eb7c7ddef84
tav <tav@espians.com>
2018-07-02 01:09:08 +0100

    storage: add Sync method to the DB interface

--- storage/memstore/memstore.go
+++ storage/memstore/memstore.go
@@ -6,6 +6,12 @@ type DB struct {
 	kv map[string][]byte
 }
 
+// Sync flushes the data to any underlying storage. It's a no-op in the case of
+// the memstore.
+func (d *DB) Sync() error {
+	return nil
+}
+
 // New initialises a new memstore DB.
 func New() *DB {
 	db := &DB{
--- storage/storage.go
+++ storage/storage.go
@@ -3,4 +3,5 @@ package storage // import "chainspace.io/prototype/storage"
 
 // DB specifies the interface implemented by all Chainspace storage methods.
 type DB interface {
+	Sync() error
 }
