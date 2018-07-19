8bd088dd86b26897eb2e5143d82ee0b7d66a856e
tav <tav@espians.com>
2018-07-19 14:45:50 +0100

    byzco: start defining base data structures

--- byzco/byzco.go
+++ byzco/byzco.go
@@ -1,2 +1,17 @@
 // Package byzco implements the core ByzCoChain consensus algorithm.
 package byzco // import "chainspace.io/prototype/byzco"
+
+// BlockID represents a specific block from a node in a way that can be used as
+// a map key.
+type BlockID struct {
+	Hash   string
+	NodeID uint64
+	Round  uint64
+}
+
+// Interpreted represents the results of interpreting a DAG for a particular
+// round.
+type Interpreted struct {
+	Blocks []BlockID
+	Round  uint64
+}
--- byzco/dag.go
+++ byzco/dag.go
@@ -1,24 +1,55 @@
 package byzco
 
-type BlockID struct {
-	Hash   string
-	NodeID uint64
-	Round  uint64
-}
+import (
+	"context"
+	"sync"
+	"time"
+)
 
 // DAG represents the directed acyclic graph that is generated from the nodes in
 // a shard broadcasting to each other.
 type DAG struct {
-	cb func(round uint64, blocks []BlockID)
+	cb    func(*Interpreted)
+	ctx   context.Context
+	mu    sync.RWMutex
+	round uint64
+	state map[BlockID][]BlockID
 }
 
-// AddEdge updates the DAG with an edge between two nodes labeled with the given
-// round.
-func (d *DAG) AddEdge(from uint64, to uint64, round uint64) {}
+func (d *DAG) prune() {
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			state := map[BlockID][]BlockID{}
+			d.mu.Lock()
+			// last := d.lastRound
+			for from, to := range d.state {
+				state[from] = to
+			}
+			d.state = state
+			d.mu.Unlock()
+		case <-d.ctx.Done():
+			return
+		}
+	}
+}
+
+// AddEdge updates the DAG with an edge between blocks.
+func (d *DAG) AddEdge(from BlockID, to []BlockID) {
+	d.mu.Lock()
+	d.state[from] = to
+	d.mu.Unlock()
+}
 
 // NewDAG instantiates a DAG for use by the broadcast/consensus mechanism.
-func NewDAG(cb func(round uint64, blocks []BlockID)) *DAG {
-	return &DAG{
-		cb: cb,
+func NewDAG(ctx context.Context, nodes []uint64, lastRound uint64, cb func(*Interpreted)) *DAG {
+	d := &DAG{
+		cb:    cb,
+		ctx:   ctx,
+		round: lastRound + 1,
+		state: map[BlockID][]BlockID{},
 	}
+	go d.prune()
+	return d
 }
