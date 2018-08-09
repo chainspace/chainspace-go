dd814b10ef4c41a6491db9d85fc24c6a56049adf
tav <tav@espians.com>
2018-08-09 18:03:51 +0100

    byzco: add some structures and remove old code

--- byzco/graph.go
+++ byzco/graph.go
@@ -4,37 +4,51 @@ import (
 	"context"
 	"sync"
 	"time"
+
+	"chainspace.io/prototype/log"
+	"chainspace.io/prototype/log/fld"
 )
 
 type blockInfo struct {
+	block BlockID
 	links []BlockID
 	min   uint64
 	max   uint64
 }
 
+// Config represents the configuration of a byzco Graph.
+type Config struct {
+	LastConsumed    uint64
+	LastInterpreted uint64
+	Nodes           []uint64
+	SelfID          uint64
+}
+
 // Graph represents the graph that is generated from the nodes in a shard
 // broadcasting to each other.
 type Graph struct {
-	c      *controller
-	cb     func(*Interpreted)
-	ctx    context.Context
-	data   map[BlockID]blockInfo
-	mu     sync.RWMutex
-	nodes  []uint64
-	quorum int
-	round  uint64
-	rounds map[uint64]map[BlockID]struct{}
-	self   uint64
-	valid  int
+	blocks   []blockInfo
+	c        *controller
+	cb       func(*Interpreted)
+	consumed uint64
+	ctx      context.Context
+	data     map[BlockID]blockInfo
+	mu       sync.RWMutex
+	nodes    []uint64
+	quorum   int
+	round    uint64
+	rounds   map[uint64]map[BlockID]struct{}
+	self     uint64
+	valid    int
 }
 
 func (g *Graph) interpret(round uint64) {
 	c := &controller{graph: g, round: round}
+	c.run()
 	g.mu.Lock()
 	g.c = c
 	g.round = round
 	g.mu.Unlock()
-	go c.run()
 }
 
 func (g *Graph) prune() {
@@ -75,40 +89,50 @@ func (g *Graph) resolve(round uint64, hashes map[uint64]string) {
 }
 
 // Add updates the graph and notifies the appropriate controllers.
-func (g *Graph) Add(block BlockID, links []BlockID) {
-	min := block.Round
-	max := block.Round
-	for _, link := range links {
-		if link.Round < min {
-			min = link.Round
-		} else if link.Round > max {
-			max = link.Round
-		}
+func (g *Graph) Add(data *BlockGraph) {
+	log.Info("Adding block to graph", fld.BlockID(data.Block))
+	for _, dep := range data.Deps {
+		log.Info("Dep:", fld.BlockID(dep.Block))
 	}
-	g.mu.Lock()
-	g.data[block] = blockInfo{
-		links: links,
-		min:   min,
-		max:   max,
-	}
-	c := g.c
-	g.mu.Unlock()
-	_ = c
+	// min := block.Round
+	// max := block.Round
+	// for _, link := range links {
+	// 	if link.Round < min {
+	// 		min = link.Round
+	// 	} else if link.Round > max {
+	// 		max = link.Round
+	// 	}
+	// }
+	// info := blockInfo{
+	// 	links: links,
+	// 	min:   min,
+	// 	max:   max,
+	// }
+	// g.mu.Lock()
+	// g.data[block] = info
+	// c := g.c
+	// g.mu.Unlock()
+	// if min >= c.round {
+	// 	e := event{block, links}
+	// 	for _, instance := range c.instances[block.NodeID] {
+	// 		instance.addEvent(e)
+	// 	}
+	// }
 }
 
 // New instantiates a Graph for use by the broadcast/consensus mechanism.
-func New(ctx context.Context, nodes []uint64, lastRound uint64, cb func(*Interpreted)) *Graph {
-	f := (len(nodes) - 1) / 3
+func New(ctx context.Context, cfg *Config, cb func(*Interpreted)) *Graph {
+	f := (len(cfg.Nodes) - 1) / 3
 	g := &Graph{
-		cb:     cb,
-		ctx:    ctx,
-		data:   map[BlockID]blockInfo{},
-		nodes:  nodes,
-		quorum: (2 * f) + 1,
-		self:   nodes[0],
-		valid:  f + 1,
+		cb:       cb,
+		consumed: cfg.LastConsumed,
+		ctx:      ctx,
+		data:     map[BlockID]blockInfo{},
+		nodes:    cfg.Nodes,
+		quorum:   (2 * f) + 1,
+		self:     cfg.SelfID,
+		valid:    f + 1,
 	}
-	g.interpret(lastRound + 1)
-	go g.prune()
+	g.interpret(cfg.LastInterpreted + 1)
 	return g
 }
--- byzco/instance.go
+++ byzco/instance.go
@@ -1,32 +1,16 @@
 package byzco
 
 import (
-	"fmt"
 	"sync"
 
 	"chainspace.io/prototype/log"
 	"chainspace.io/prototype/log/fld"
 )
 
-const (
-	unknownState status = iota
-	committed
-	inCommit
-	inPrePrepare
-	inPrepare
-	initialState
-)
-
-type event struct{}
-
-type handler func(i *instance) (status, error)
-
-// Instance represents a state machine for determining the block for a given node.
+// instance determines the block for a given node.
 type instance struct {
-	actions     map[status]handler
-	c           *controller
 	cond        *sync.Cond
-	events      []*event
+	events      []interface{}
 	hash        string
 	log         *log.Logger
 	mu          sync.Mutex
@@ -34,55 +18,18 @@ type instance struct {
 	perspective uint64
 	round       uint64
 	status      status
-	transitions map[transition]handler
 }
 
-func (i *instance) addEvent(e *event) {
+func (i *instance) addEvent(e interface{}) {
 	i.mu.Lock()
 	i.events = append(i.events, e)
 	i.mu.Unlock()
 	i.cond.Signal()
 }
 
-func (i *instance) handleEvent(e *event) {
-	// First, process the event by updating any relevant state.
-
-	// Then, run actions and transition handlers until we can progress no
-	// further.
-	for {
-		if i.status == committed {
-			return
-		}
-		action, exists := i.actions[i.status]
-		if !exists {
-			i.log.Fatal("Unable to find an action for the current state", fld.Status(i.status))
-		}
-		status, err := action(i)
-		if err != nil {
-			i.log.Fatal("Unable to run the action", fld.Status(i.status), fld.Err(err))
-		}
-		if status == i.status {
-			return
-		}
-		for {
-			handler, exists := i.transitions[transition{i.status, status}]
-			if !exists {
-				i.status = status
-				break
-			}
-			next, err := handler(i)
-			if err != nil {
-				i.log.Fatal("Unable to run the transition handler", fld.FromState(i.status), fld.ToState(status), fld.Err(err))
-			}
-			i.status = status
-			status = next
-		}
-	}
-}
-
 func (i *instance) release() {
-	<-i.c.ctx.Done()
-	i.cond.Signal()
+	// <-i.c.ctx.Done()
+	// i.cond.Signal()
 }
 
 func (i *instance) run() {
@@ -90,60 +37,33 @@ func (i *instance) run() {
 		i.mu.Lock()
 		for len(i.events) == 0 {
 			i.cond.Wait()
-			select {
-			case <-i.c.ctx.Done():
-				i.mu.Unlock()
-				return
-			default:
-			}
+			// select {
+			// case <-i.c.ctx.Done():
+			// 	i.mu.Unlock()
+			// 	return
+			// default:
+			// }
 		}
 		e := i.events[0]
 		i.events = i.events[1:]
 		i.mu.Unlock()
-		i.handleEvent(e)
-		if i.status == committed {
-			i.c.callback(i.perspective, i.node, i.hash)
-			break
-		}
-	}
-}
-
-type transition struct {
-	from status
-	to   status
-}
-
-type status uint8
-
-func (s status) String() string {
-	switch s {
-	case unknownState:
-		return "unknownState"
-	case committed:
-		return "committed"
-	case inCommit:
-		return "inCommit"
-	case inPrePrepare:
-		return "inPrePrepare"
-	case inPrepare:
-		return "inPrepare"
-	case initialState:
-		return "initialState"
-	default:
-		panic(fmt.Errorf("byzco: unknown status: %d", s))
+		_ = e
+		// i.handleEvent(e)
+		// if i.status == committed {
+		// 	i.c.callback(i.perspective, i.node, i.hash)
+		// 	break
+		// }
 	}
 }
 
 func newInstance(c *controller, perspective uint64, node uint64, round uint64) *instance {
 	i := &instance{
-		actions:     actions,
-		c:           c,
+		// c:           c,
 		log:         log.With(fld.Round(round), fld.Perspective(perspective), fld.NodeID(node)),
 		node:        node,
 		perspective: perspective,
 		round:       round,
-		status:      initialState,
-		transitions: transitions,
+		// status:      initialState,
 	}
 	i.cond = sync.NewCond(&i.mu)
 	go i.release()
