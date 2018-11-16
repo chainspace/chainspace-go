6c8b61917ac384fe2d784ec4ae8024aaeb5583be
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2018-11-16 14:52:24 +0000

    blockmania: testing String() func

--- blockmania/blockmania_test.go
+++ blockmania/blockmania_test.go
@@ -1,6 +1,8 @@
 package blockmania_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -10,26 +12,47 @@ import (
 var _ = Describe("Blockmania", func() {
 
 	var (
-		block BlockID
+		blockID BlockID
 	)
 
 	BeforeEach(func() {
-		block = BlockID{}
+		blockID = BlockID{}
+	})
+
+	Describe("String", func() {
+		Context("A valid BlockID", func() {
+			BeforeEach(func() {
+				blockID.Hash = "foofoofoofoo" // minimum 12 char hash :)
+			})
+
+			It("should return a formatted string", func() {
+
+				var result = fmt.Sprintf("%v | %v | %v", blockID.Node, blockID.Round, "666F6F666F6F")
+				Expect(blockID.String()).To(Equal(result))
+			})
+		})
+
+		Context("An invalid BlockID", func() {
+			It("should return a formatted string", func() {
+				var result = fmt.Sprintf("%v | %v", blockID.Node, blockID.Round)
+				Expect(blockID.String()).To(Equal(result))
+			})
+		})
 	})
 
 	Describe("Valid", func() {
 		Context("with an empty hash", func() {
 			It("should be invalid", func() {
-				Expect(block.Valid()).To(Equal(false))
+				Expect(blockID.Valid()).To(Equal(false))
 			})
 		})
 
 		Context("with an populated hash", func() {
 			BeforeEach(func() {
-				block.Hash = "foo"
+				blockID.Hash = "foo"
 			})
 			It("should be valid", func() {
-				Expect(block.Valid()).To(Equal(true))
+				Expect(blockID.Valid()).To(Equal(true))
 			})
 		})
 	})
