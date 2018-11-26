5f27096481ad039dfb2d03e033a295811b6ab9b2
Stuart Chinery <stuart.chinery@gmail.com>
2018-11-26 13:18:45 +0000

    Added tests for sbac/strings

--- sbac/strings.go
+++ sbac/strings.go
@@ -1,22 +1,26 @@
 package sbac // import "chainspace.io/prototype/sbac"
 
+// StringsSlice ...
 type StringsSlice []*Strings
 
-func (m *Strings) AsSlice() []string {
-	return m.Strs
+// AsSlice returns a map of Strings as a slice
+func (s *Strings) AsSlice() []string {
+	return s.Strs
 }
 
-func (m StringsSlice) AsSlice() [][]string {
+// AsSlice returns a map of StringsSlice as a slice
+func (ss StringsSlice) AsSlice() [][]string {
 	out := [][]string{}
-	for _, strs := range m {
+	for _, strs := range ss {
 		out = append(out, strs.AsSlice())
 	}
 	return out
 }
 
-func (ss StringsSlice) FromSlice(lsstrs [][]string) []*Strings {
+// FromSlice returns a map of Strings from a StringsSlice
+func (ss StringsSlice) FromSlice(mapOfStrs [][]string) []*Strings {
 	out := []*Strings{}
-	for _, strs := range lsstrs {
+	for _, strs := range mapOfStrs {
 		out = append(out, &Strings{Strs: strs})
 	}
 	return out
