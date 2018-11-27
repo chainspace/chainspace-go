afa194281b7e99021f98a72f2453554bf29702f8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2018-11-27 11:47:30 +0000

    blockmania: noted tests are only for BlockID so far

--- blockmania/blockmania_test.go
+++ blockmania/blockmania_test.go
@@ -8,59 +8,62 @@ import (
 )
 
 var _ = Describe("Blockmania", func() {
-	var (
-		blockID BlockID
-	)
+	Describe("BlockID", func() {
+		var (
+			blockID BlockID
+		)
 
-	BeforeEach(func() {
-		blockID = BlockID{}
+		BeforeEach(func() {
+			blockID = BlockID{}
+		})
+
+		Describe("String", func() {
+			Context("A valid BlockID", func() {
+				BeforeEach(func() {
+					blockID.Hash = "foofoofoofoo" // minimum 12 char hash :)
+				})
+
+				It("should return a formatted string with the node, round, and block hash", func() {
+					actual := blockID.String()
+					expected := fmt.Sprintf("%v | %v | %v", blockID.Node, blockID.Round, "666F6F666F6F")
+
+					Expect(actual).To(Equal(expected))
+				})
+			})
+
+			Context("An invalid BlockID", func() {
+				It("should return a formatted string with only the node and round", func() {
+					actual := blockID.String()
+					expected := fmt.Sprintf("%v | %v", blockID.Node, blockID.Round)
+
+					Expect(actual).To(Equal(expected))
+				})
+			})
+		})
+
+		Describe("Valid", func() {
+			Context("with an empty hash", func() {
+				It("should be invalid", func() {
+					actual := blockID.Valid()
+					expected := false
+
+					Expect(actual).To(Equal(expected))
+				})
+			})
+
+			Context("with a populated hash", func() {
+				BeforeEach(func() {
+					blockID.Hash = "foo"
+				})
+
+				It("should be valid", func() {
+					actual := blockID.Valid()
+					expected := true
+
+					Expect(actual).To(Equal(expected))
+				})
+			})
+		})
 	})
 
-	Describe("String", func() {
-		Context("A valid BlockID", func() {
-			BeforeEach(func() {
-				blockID.Hash = "foofoofoofoo" // minimum 12 char hash :)
-			})
-
-			It("should return a formatted string with the node, round, and block hash", func() {
-				actual := blockID.String()
-				expected := fmt.Sprintf("%v | %v | %v", blockID.Node, blockID.Round, "666F6F666F6F")
-
-				Expect(actual).To(Equal(expected))
-			})
-		})
-
-		Context("An invalid BlockID", func() {
-			It("should return a formatted string with only the node and round", func() {
-				actual := blockID.String()
-				expected := fmt.Sprintf("%v | %v", blockID.Node, blockID.Round)
-
-				Expect(actual).To(Equal(expected))
-			})
-		})
-	})
-
-	Describe("Valid", func() {
-		Context("with an empty hash", func() {
-			It("should be invalid", func() {
-				actual := blockID.Valid()
-				expected := false
-
-				Expect(actual).To(Equal(expected))
-			})
-		})
-
-		Context("with an populated hash", func() {
-			BeforeEach(func() {
-				blockID.Hash = "foo"
-			})
-
-			It("should be valid", func() {
-				actual := blockID.Valid()
-				expected := true
-
-				Expect(actual).To(Equal(expected))
-			})
-		})
-	})
 })
