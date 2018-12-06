139da601f80b6adb5c378572a26be9cc667aeb03
jeremyletang <letang.jeremy@gmail.com>
2018-12-06 14:38:32 +0000

    move the fixture in the sbac folder

---
+++ sbac/api/fixtures_test.go
@@ -0,0 +1,41 @@
+package api_test
+
+import (
+	sbacapi "chainspace.io/prototype/sbac/api"
+)
+
+var transactionFixture = sbacapi.Transaction{
+	Mappings: map[string]interface{}{
+		"4S8G4OhhLcXFgzXvnFoGPdEC/QtjbX6zQHJmeevOPhE=": "foobar",
+	},
+	Signatures: nil,
+	Traces: []sbacapi.Trace{{
+		ContractID: "dummy",
+		Dependencies: []sbacapi.Dependency{{
+			ContractID:               "dummy",
+			Dependencies:             nil,
+			InputObjectVersionIDs:    []string{"4S8G4OhhLcXFgzXvnFoGPdEC/QtjbX6zQHJmeevOPhE="},
+			InputReferenceVersionIDs: []string{"4S8G4OhhLcXFgzXvnFoGPdEC/QtjbX6zQHJmeevOPhE="},
+			OutputObjects: []sbacapi.OutputObject{
+				{
+					Labels: []string{"foo"},
+					Object: "thisissomethingmagical!",
+				},
+			},
+			Parameters: nil,
+			Procedure:  "dummy_ok",
+			Returns:    nil,
+		}},
+		InputObjectVersionIDs:    []string{"4S8G4OhhLcXFgzXvnFoGPdEC/QtjbX6zQHJmeevOPhE="},
+		InputReferenceVersionIDs: []string{"4S8G4OhhLcXFgzXvnFoGPdEC/QtjbX6zQHJmeevOPhE="},
+		OutputObjects: []sbacapi.OutputObject{
+			{
+				Labels: []string{"foo"},
+				Object: "thisissomethingmagical!",
+			},
+		},
+		Parameters: nil,
+		Procedure:  "dummy_ok",
+		Returns:    nil,
+	}},
+}
